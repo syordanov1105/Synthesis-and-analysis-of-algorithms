81c64135b185437ac218c1dfc22065d102fcb7be
Stanislav Y <syordanov1105@gmail.com>
2023-09-04 13:47:35 +0300

    Implement quickSort

---
+++ Sorting/quickSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+int partition(int array[], int low, int high) {
+
+  int pivot = array[high];
+  int i = (low - 1);
+
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+
+      i++;
+
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  swap(&array[i + 1], &array[high]);
+
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+
+    int pi = partition(array, low, high);
+
+    quickSort(array, low, pi - 1);
+
+    quickSort(array, pi + 1, high);
+  }
+}
+
+
+int main() {
+    int array[] = {0,2,1,5,4,3,9,7,8,6};
+    quickSort(array, 0, 9);
+
+    for(int i=0; i<10; i++){
+        printf("%d\t", array[i]);
+    }
+}
